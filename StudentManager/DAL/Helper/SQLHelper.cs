152fb221207c45bc3b319b2f384fd2735e04bd8e
shark <570039068@qq.com>
2025-03-02 20:04:38 +0800

    框架

---
+++ StudentManager/DAL/Helper/SQLHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace DAL
+{
+    /// <summary>
+    /// 数据库访问通用类
+    /// </summary>
+    internal class SQLHelper
+    {
+    }
+}
---
+++ StudentManager/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    /// <summary>
+    /// 学员实体类
+    /// </summary>
+    public class Student
+    {
+        public int StudentId { get; set; } // 学生ID
+        public string StudentName { get; set; } // 学生姓名
+        public string Gender { get; set; } // 性别
+        public DateTime Birthday { get; set; } // 生日
+        public string StudentIdNo { get; set; } // 身份证号
+        public string CardNo { get; set; } // 考勤卡号
+        public int Age { get; set; } // 年龄
+        public string StuImage {  get; set; }//照片
+        public string PhoneNumber { get; set; } // 电话号码
+        public string StudentAddress { get; set; } // 地址
+        public int ClassId { get; set; } // 班级外键
+
+    }
+}
