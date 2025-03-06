9f6ce75abf3ec4c056a917c5d4f5ad19805b3e47
shark <570039068@qq.com>
2025-03-06 21:21:10 +0800

    day4

--- StudentManager/DAL/ScoreListService.cs
+++ StudentManager/DAL/ScoreListService.cs
@@ -35,7 +35,7 @@ namespace DAL
             string sql = "select Students.StudentId,StudentName,ClassName,Gender,PhoneNumber,CSharp,SQLServerDB from Students";
                sql += " inner join StudentClass on StudentClass.ClassId=Students.ClassId ";
                sql += " inner join ScoreList on ScoreList.StudentId=Students.StudentId ";
-            if(className != null&&className.Length!=0)
+            if(className != null&&className.Length!=0)//这里添加判断,当查询全校成绩时,不使用where
             {
                 sql+= " where ClassName='" + className + "'";
             }
@@ -62,10 +62,69 @@ namespace DAL
 
 
         //根据班级(全校)统计成绩信息
+        public Dictionary<string, string> QueryScoreInfo(int classId)
+        {
+            string sql = "select stuCount=count(*),avgCSharp=avg(CSharp),avgDB=avg(SQLServerDB) from ScoreList ";
+            sql += "inner join Students on Students.StudentId=ScoreList.StudentId";
+            if( classId !=-1)
+            {
+                sql += string.Format(" where ClassId={0}", classId);
+            }
+            sql += ";select absentCount=count(*) from Students where StudentId not in";
+            sql += "(select StudentId from ScoreList)";
+            if (classId!=-1)
+            {
+                sql += string.Format(" and ClassId={0}", classId);
+            }
+            
+            SqlDataReader objReader = SQLHelper.GetReader(sql);
+            Dictionary<string, string> scoreInfo = null;
+            if (objReader.Read())//读取考试成绩统计结果
+            {
+                scoreInfo = new Dictionary<string, string>();
+                scoreInfo.Add("stuCount", objReader["stucount"].ToString());
+                scoreInfo.Add("avgCSharp", objReader["avgCSharp"].ToString());
+                scoreInfo.Add("avgDB", objReader["avgDB"].ToString());
+            }
+            if (objReader.NextResult())//读取缺考人数列表
+            {
+                if (objReader.Read())
+                {
+                    scoreInfo.Add("absentCount", objReader["absentCount"].ToString());
+                }
+            }
+            objReader.Close();
+            return scoreInfo;
+
+
+        }
+
+
+
 
 
 
         //根据班级(全校)查询缺考人员列表
+        public List<string> GetStudentsque(int classId)
+        {
+            string sql = "select StudentName from Students where StudentId not in ";
+            sql += "(select StudentId from ScoreList)";
+            if(classId != -1)
+            {
+                sql += string.Format(" and ClassId='{0}'", classId);
+            }
+            SqlDataReader objReader = SQLHelper.GetReader(sql);
+            List<string> list = new List<string>();
+            while (objReader.Read())
+            {
+                list.Add(objReader["StudentName"].ToString());
+            }
+            objReader.Close();
+            return list;
+
+        }
+
+
 
     }
 
--- StudentManager/StudentManager/FrmScoreManage.cs
+++ StudentManager/StudentManager/FrmScoreManage.cs
@@ -1,3 +1,4 @@
+using DAL;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -11,15 +12,52 @@ namespace StudentManager
 {
     public partial class FrmScoreManage : Form
     {     
+        private StudentClassService studentClassService=new StudentClassService();
+        private ScoreListService scoreListService=new ScoreListService();   
         public FrmScoreManage()
         {
             InitializeComponent();
-        
+            this.cboClass.DataSource = studentClassService.GetAllClass();
+            this.cboClass.DisplayMember = "ClassName";//设置下拉框显示内容
+            this.cboClass.ValueMember = "ClassId";//设置下拉框显示内容对应的value
+            this.cboClass.SelectedIndex = -1;//
+
         }     
         //根据班级查询      
         private void cboClass_SelectedIndexChanged(object sender, EventArgs e)
         {
-          
+            this.dgvScoreList.AutoGenerateColumns=false;//不显示datagripview控件中未封装的内容
+          //if(this.cboClass.SelectedIndex != -1)
+          //  {
+          //      MessageBox.Show("请选择查询的班级", "提示");
+          //      return;
+          //  }
+          //  this.dgvScoreList.DataSource = scoreListService.GetScoreList(this.cboClass.Text.Trim());
+          //查询数据集
+            if (this.cboClass.SelectedIndex != -1)
+            {
+                this.dgvScoreList.DataSource = scoreListService.GetScoreList(this.cboClass.Text.Trim());
+            }
+            //查询统计结果
+
+
+            Dictionary<string, string> dic = scoreListService.QueryScoreInfo(Convert.ToInt32(this.cboClass.SelectedValue));
+            this.lblAttendCount.Text = dic["stuCount"];
+            this.lblCSharpAvg.Text = dic["avgCSharp"];
+            this.lblDBAvg.Text = dic["avgDB"];
+            this.lblCount.Text = dic["absentCount"];
+            //显示缺考人员姓名
+            List<string> list = scoreListService.GetStudentsque(Convert.ToInt32(this.cboClass.SelectedValue));
+            this.lblList.Items.Clear();
+            if (list.Count == 0) this.lblList.Items.Add("没有缺考");
+            else
+            {
+                lblList.Items.AddRange(list.ToArray());
+                //   lblList.DataSource = list;
+            }
+
+
+
         }
         //关闭
         private void btnClose_Click(object sender, EventArgs e)
@@ -29,7 +67,8 @@ namespace StudentManager
         //统计全校考试成绩
         private void btnStat_Click(object sender, EventArgs e)
         {          
-          
+                this.dgvScoreList.DataSource = scoreListService.GetScoreList("");
+
         }
 
         private void dgvScoreList_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
